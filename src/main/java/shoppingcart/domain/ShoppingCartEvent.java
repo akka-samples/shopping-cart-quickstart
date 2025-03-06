0c911906e7ccd395d1bbf50fc39088b8ad613064
lightbend-tools-secure <lightbend-tools-secure@lightbend.com>
2025-03-06 07:50:21 +0000

    chore: bump SDK version to 3.2.0

--- src/main/java/shoppingcart/domain/ShoppingCartEvent.java
+++ src/main/java/shoppingcart/domain/ShoppingCartEvent.java
@@ -1,10 +1,6 @@
-// tag::top[]
 package shoppingcart.domain;
 
 import akka.javasdk.annotations.TypeName;
-// end::top[]
-// tag::events[]
-// tag::domain[]
 
 public sealed interface ShoppingCartEvent { // <1>
 
@@ -12,7 +8,6 @@ public sealed interface ShoppingCartEvent { // <1>
   record ItemAdded(ShoppingCart.LineItem item) implements ShoppingCartEvent {
   }
 
-// end::domain[]
 
   @TypeName("item-removed")
   record ItemRemoved(String productId) implements ShoppingCartEvent {
@@ -21,7 +16,4 @@ public sealed interface ShoppingCartEvent { // <1>
   @TypeName("checked-out")
   record CheckedOut() implements ShoppingCartEvent {
   }
-// tag::domain[]
 }
-// end::domain[]
-// end::events[]
