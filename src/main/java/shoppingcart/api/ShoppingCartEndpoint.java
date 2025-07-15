686c6d3223283ccfdc8d7b7685669c5cce563ead
lightbend-tools-secure <lightbend-tools-secure@lightbend.com>
2025-07-15 14:00:49 +0000

    chore: bump SDK version to 3.4.4

--- src/main/java/shoppingcart/api/ShoppingCartEndpoint.java
+++ src/main/java/shoppingcart/api/ShoppingCartEndpoint.java
@@ -3,9 +3,9 @@ package shoppingcart.api;
 import akka.http.javadsl.model.HttpResponse;
 import akka.javasdk.annotations.Acl;
 import akka.javasdk.annotations.http.Delete;
-import akka.javasdk.annotations.http.Post;
-import akka.javasdk.annotations.http.HttpEndpoint;
 import akka.javasdk.annotations.http.Get;
+import akka.javasdk.annotations.http.HttpEndpoint;
+import akka.javasdk.annotations.http.Post;
 import akka.javasdk.annotations.http.Put;
 import akka.javasdk.client.ComponentClient;
 import akka.javasdk.http.HttpResponses;
@@ -17,7 +17,8 @@ import shoppingcart.domain.ShoppingCart;
 
 
 // Opened up for access from the public internet to make the sample service easy to try out.
-// For actual services meant for production this must be carefully considered, and often set more limited
+// For actual services meant for production this must be carefully considered,
+// and often set more limited
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
 @HttpEndpoint("/carts") // <1>
 public class ShoppingCartEndpoint {
@@ -34,26 +35,30 @@ public class ShoppingCartEndpoint {
   @Get("/{cartId}") // <3>
   public ShoppingCart get(String cartId) {
     logger.info("Get cart id={}", cartId);
-    return componentClient.forEventSourcedEntity(cartId) // <4>
-        .method(ShoppingCartEntity::getCart)
-        .invoke(); // <5>
+    return componentClient
+      .forEventSourcedEntity(cartId) // <4>
+      .method(ShoppingCartEntity::getCart)
+      .invoke(); // <5>
   }
 
 
   @Put("/{cartId}/item") // <6>
   public HttpResponse addItem(String cartId, ShoppingCart.LineItem item) {
     logger.info("Adding item to cart id={} item={}", cartId, item);
-    componentClient.forEventSourcedEntity(cartId)
+    componentClient
+      .forEventSourcedEntity(cartId)
       .method(ShoppingCartEntity::addItem)
       .invoke(item);
     return HttpResponses.ok(); // <7>
   }
 
 
+
   @Delete("/{cartId}/item/{productId}")
   public HttpResponse removeItem(String cartId, String productId) {
     logger.info("Removing item from cart id={} item={}", cartId, productId);
-    componentClient.forEventSourcedEntity(cartId)
+    componentClient
+      .forEventSourcedEntity(cartId)
       .method(ShoppingCartEntity::removeItem)
       .invoke(productId);
     return HttpResponses.ok();
@@ -62,10 +67,10 @@ public class ShoppingCartEndpoint {
   @Post("/{cartId}/checkout")
   public HttpResponse checkout(String cartId) {
     logger.info("Checkout cart id={}", cartId);
-    componentClient.forEventSourcedEntity(cartId)
+    componentClient
+      .forEventSourcedEntity(cartId)
       .method(ShoppingCartEntity::checkout)
       .invoke();
     return HttpResponses.ok();
   }
-
 }
